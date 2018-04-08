be396910783c6294c6c713561f4b89e97a512f39
RichmanRick <richardsc210@gmail.com>
2018-04-07 22:41:40 -0700

    created a method to do the string to list conversion

--- ComparePercentage.py
+++ ComparePercentage.py
@@ -1,5 +1,12 @@
-string_set = "0.9940024: 0.0059757535: 2.0527972e-05: 9.947543e-07: 3.1465524e-07: "
-stringArray = string_set.split(": ")
+string_numberset = "0.9940024: 0.0059757535: 2.0527972e-05: 9.947543e-07: 3.1465524e-07: "
 
-print stringArray
-#['0.9940024', '0.0059757535', '2.0527972e-05', '9.947543e-07', '3.1465524e-07', '']
+#method for converting the string of numbers into a list of numbers as strings
+def string2list(string):
+	string = string.strip()
+	string = string[0:len(string) - 1].split(": ")
+	return string
+
+
+converted = string2list(string_numberset)
+print converted #['0.9940024', '0.0059757535', '2.0527972e-05', '9.947543e-07', '3.1465524e-07']
+
