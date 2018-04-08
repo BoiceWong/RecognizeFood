2398cd162b427166fb2de90f56bfa7c130629267
RichmanRick <richardsc210@gmail.com>
2018-04-07 21:49:45 -0700

    put float values into a string list

---
+++ ComparePercentage.py
@@ -0,0 +1,4 @@
+string_set = "0.9940024: 0.0059757535: 2.0527972e-05: 9.947543e-07: 3.1465524e-07: "
+stringArray = string_set.split(": ")
+
+print stringArray
--- Sunhack.py
+++ Sunhack.py
@@ -6,3 +6,4 @@ import numpy as np
 import tensorflow as tf
 import time
 import data_helpers
+
