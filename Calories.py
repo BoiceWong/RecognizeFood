5891de27169fda9261a2828d14565441ab5f9a97
monacook <cook.mona@gmail.com>
2018-04-07 22:53:49 -0700

    Mona comment

---
+++ Calories.py
@@ -0,0 +1,15 @@
+#// get Calories(string), instiatate them 
+#// pick 5 foods, average count calary 
+#// Sirloin steak - 280.8 
+#// mac n cheese - calories 219.0 
+#// Blue doritos - 150 calories 
+#// Gourmet Popcorn - 70 calories 
+#// Chewy peanut butter choco chip - 100 calories 
+#// make a list with a bunch of local variables 
+# string name = "Steak";
+String sirloinSteak = "280.08"
+String macNcheese = "219.0"
+String blueDoritos = "150"
+String gourmetPopcorn = "70"
+String chewBar = "100"
+
